fa8064d65630381255664746035ab2e5a6872126
maria <ermmmaria@gmail.com>
2023-03-17 18:25:06 +0100

    Cleaning up the scripts
    Renamed variables, separated script in modules, removed unused lines.

--- Patterns/avgPattern.m
+++ Patterns/avgPattern.m
@@ -2,51 +2,53 @@
 % Load spatial patterns, take magnitude part, interpolate missing channels,
 % reorder channels to a common order, normalise by subject, plot the
 % average across subjects
+
 clear
 addpath('C:\Users\BNPPC08\Desktop\Maria\matlab\toolboxes\eeglab14_1_2b')
 addpath 'C:\Users\BNPPC08\Desktop\Maria\matlab\toolboxes\MatlabFns\Colourmaps'
 eeglab
 load("allPatterns_15-03-2023.mat");
 datTable = readtable('W:\Projects\2018-12 POSTHOCSOURCE Project\analysis_maria\CSPRepo\cspAnalysis\REFTEP_list.xlsx', 'Basic', 1);
+
 %% interpolate missing channels and reorder channels to a common order
 clear allPatternsHigh allPatternsLow
-subjects = [1:7,9:20];
-timeIdx = 1;
-freqIdx = 1;
 
+subjects = [1:7,9:20]; %select significant subjects
 
 for idxSub = 1:length(subjects)
-    clear chanlocs0 rmch
     
-    iSub = subjects(idxSub);
+    idSub = subjects(idxSub);
 
-    load(char(datTable.Data(iSub)), 'chanlocs0');
-%     load(char(datTable.Data(iSub)), 'EEG');
-    if ismember(datTable.Struct(iSub),'Xclean2')
-        load(char(datTable.Data(iSub)), 'rmch');
-    elseif ismember(datTable.Struct(iSub),'XAl')
-        load(char(datTable.Data(iSub)), 'badCh');
+    %get the list of channels of the subject
+    load(char(datTable.Data(idSub)), 'chanlocs0');
+    if ismember(datTable.Struct(idSub),'Xclean2')
+        load(char(datTable.Data(idSub)), 'rmch');
+    elseif ismember(datTable.Struct(idSub),'XAl')
+        load(char(datTable.Data(idSub)), 'badCh');
         rmch = badCh;
     end
 
-    V1inv = allV1inv{iSub};
-    V2inv = allV2inv{iSub};
+    %get the patterns of the subject
+    VHinv = allV1inv{idSub};
+    VLinv = allV2inv{idSub};
 
-
-    EEG.data = abs(V1inv(:,1));
+    %prepare patterns of High condition
+    EEG.data = abs(VHinv(:,1)); %take magnitude of the first pattern
     EEG.chanlocs = chanlocs0(~rmch);
     EEG.pnts = 1;
     EEG.trials = 1;
     EEG.nbchan = sum(~rmch);
-    EEG = pop_interp( EEG, chanlocs,'spherical');
+    EEG = pop_interp( EEG, chanlocs,'spherical'); %interpolate missing channels
 
+    %reorder data by a standard channel order
     [~,reorderingIdx] = ismember(lower({chanlocs.labels}), lower({EEG.chanlocs.labels}));
     EEG.chanlocs = EEG.chanlocs(reorderingIdx);
     EEG.data     = EEG.data(reorderingIdx);
 
     allPatternsHigh(:,idxSub) = EEG.data;
 
-    EEG.data = abs(V2inv(:,1));
+    %prepare patterns of Low condition
+    EEG.data = abs(VLinv(:,1));
     EEG.chanlocs = chanlocs0(~rmch);
     EEG.pnts = 1;
     EEG.trials = 1;
@@ -60,6 +62,7 @@ for idxSub = 1:length(subjects)
     allPatternsLow(:,idxSub) = EEG.data;
 
 end
+
 %% normalise by sum
 for idxSub = 1:length(subjects)
     allPatternsHigh(:,idxSub) = allPatternsHigh(:,idxSub)./sum(allPatternsHigh(:,idxSub),'all');
@@ -76,18 +79,19 @@ for idxSub = 1:length(subjects)
     allPatternsLow(:,idxSub) = (allPatternsLow(:,idxSub)-mean(allPatternsHigh(:,idxSub))./norm(allPatternsHigh(:,idxSub)));
 end
 %% plot average pattern
+avgPatternHigh = mean(allPatternsHigh,2);
+avgPatternLow = mean(allPatternsLow,2);
+
 figure; tiledlayout(1,2)
 nexttile
-topoplot(mean(allPatternsHigh,2), chanlocs,'style','map','maplimits','minmax','electrodes','off');
+topoplot(avgPatternHigh, chanlocs,'style','map','maplimits','minmax','electrodes','off');
 colorcet('L3','reverse',1)
 title("Avg pattern for High")
-%caxis([0 0.07])
 
 nexttile
-topoplot(mean(allPatternsLow,2), chanlocs,'style','map','maplimits','minmax','electrodes','off');
+topoplot(avgPatternLow, chanlocs,'style','map','maplimits','minmax','electrodes','off');
 colorcet('L3','reverse',1)
 title("Avg pattern for Low")
-%caxis([0.15 0.51])
 
 
 
